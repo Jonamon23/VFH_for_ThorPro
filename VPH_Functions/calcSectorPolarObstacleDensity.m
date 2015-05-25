76b41aaa3e19309f99ff626178101c35b44ba4bd
Jonamon23 <jkreska23@gmail.com>
2015-05-25 13:04:11 -0400

    Version: 1.1
    Removed Debugging. Added Header

--- VPH_Functions/calcSectorPolarObstacleDensity.m
+++ VPH_Functions/calcSectorPolarObstacleDensity.m
@@ -1,19 +1,27 @@
-function H_p_k = calcSectorPolarObstacleDensity(h_p,m)
+function H_p_k = calcSectorPolarObstacleDensity(h_p,m,sector)
+%%%%%
+% Updated by: Jonathon Kreska
+% Version Date: May 25, 2015
+% Version: 1.1
+% 
+% Changelog:
+%  1.0: Initial Release
+%  1.1: Removed Debugging.
+%
+%
+%%%%%
 
-global r_rs lidar sector;
+global r_rs lidar;
 
 enlargement_angle = (180/pi)*real(asin(r_rs./h_p));
-% assignin('base','enlargement_angle',enlargement_angle);
 
 lidar_angles = (lidar.min_angle:lidar.resolution:lidar.max_angle-lidar.resolution) + (lidar.max_angle+1);
-% assignin('base','lidar_angles',lidar_angles);
 
 LEFT = 1; RIGHT = 2; DANGER = 3;
 expanded_h_p(:,LEFT) = lidar_angles - enlargement_angle;
 expanded_h_p(:,RIGHT) = lidar_angles + enlargement_angle;
 
 expanded_h_p(expanded_h_p<0) = 0; % puts negative values into sector 1
-% assignin('base','expanded_h_p',expanded_h_p);
 
 h_prime = floor(expanded_h_p/sector.alpha)+1; % translate angles to sectors
 
@@ -23,13 +31,11 @@ h_prime(h_prime>sector.count) = sector.count; % limit sectors to max num of sect
 %m(round(4*end/5):end) = .8*m(round(4*end/5):end);
 
 h_prime = [h_prime,m']; % add danger vlaues to third column
-% assignin('base','h_prime',h_prime);
 
 H_p = zeros(length(lidar_angles),sector.count); % init array for speed
 
 for i = 1:length(h_prime)
   H_p(i,h_prime(i,LEFT):h_prime(i,RIGHT)) = h_prime(i,DANGER); % do magic
 end 
-% assignin('base','H_p',H_p);
 
 H_p_k = sum(H_p); % Sum each column's dangers into a single row
